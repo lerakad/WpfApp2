c9cfaead2e69f5ec335f47652ff88eee0b67bf28
egor_ <egor_@DESKTOP-HHFVNP0>
2024-01-22 16:22:11 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/главная.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для главная.xaml
+    /// </summary>
+    public partial class главная : Window
+    {
+        public главная()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            главная главная = new главная();
+            главная.Show();
+            this.Close();
+        }
+
+        private void Page1_Click(object sender, RoutedEventArgs e)
+        {
+            материал материал = new материал();
+            материал.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp2/материал.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для материал.xaml
+    /// </summary>
+    public partial class материал : Window
+    {
+        public материал()
+        {
+            InitializeComponent();
+            DGrid.ItemsSource = KostinaVDEntities1.GetContext().Table_Materiali.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            главная главная = new главная();
+            главная.Show();
+            this.Close();
+        }
+
+        private void DGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
